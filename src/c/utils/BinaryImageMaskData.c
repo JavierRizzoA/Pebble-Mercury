984b8a5051113b6debc98b01ca33319f250c3732
Javier Rizzo-Aguirre <javierrizzoa@gmail.com>
2026-01-03 15:55:02 -0800

    Huge performance improvements

--- src/c/utils/BinaryImageMaskData.c
+++ src/c/utils/BinaryImageMaskData.c
@@ -14,8 +14,7 @@ bool binary_image_mask_data_is_filled_or_adjacent(BinaryImageMaskData* bimd, int
 struct BinaryImageMaskData* binary_image_mask_data_create(GSize size) {
   BinaryImageMaskData *bimd = (BinaryImageMaskData*) malloc(sizeof(BinaryImageMaskData));
   bimd->size = size;
-  bimd->data = (uint8_t*)malloc(size.w * size.h / 8.0 + 0.9);
-  binary_image_mask_data_clear_region(bimd, GRect(0, 0, size.w, size.h), false);
+  bimd->data = (uint8_t*)calloc(((size.w * size.h) + 7) >> 3, 1);
   return bimd;
 }
 
@@ -33,54 +32,65 @@ void binary_image_mask_data_destroy(BinaryImageMaskData* bimd) {
 struct BinaryImageMaskData* binary_image_mask_data_create_from_resource(GSize size, uint32_t resource_id) {
   BinaryImageMaskData *bimd = binary_image_mask_data_create(size);
   ResHandle handle = resource_get_handle(resource_id);
-  resource_load(handle, bimd->data, bimd->size.w * bimd->size.h / 8.0 + 0.9);
+  resource_load(handle, bimd->data, ((bimd->size.w * bimd->size.h) + 7) >> 3);
   return bimd;
 }
 
 void binary_image_mask_data_set_bit(BinaryImageMaskData* bimd, int x, int y, bool val) {
-  uint8_t byte = bimd->data[(bimd->size.w * y + x) / 8];
-  int reminder = (bimd->size.w * y + x) % 8;
-  uint8_t mask = 1 << reminder;
-  bimd->data[(bimd->size.w * y + x) / 8] = (byte & ~mask) | (((val) ? 0b11111111 : 0) & mask);
+  size_t index = (size_t)bimd->size.w * y + x;
+  uint8_t *byte_ptr = &bimd->data[index >> 3];
+  uint8_t mask = 1 << (index & 7);
+
+  if (val) {
+    *byte_ptr |= mask;   // set bit
+  } else {
+    *byte_ptr &= ~mask;  // clear bit
+  }
 }
 
 void binary_image_mask_data_clear_region(BinaryImageMaskData* bimd, GRect region, bool centered) {
-  GPoint offset = GPoint(0, 0);
-  if (centered) {
-    offset = GPoint(region.size.w / 2, region.size.h / 2);
-  }
+  GPoint offset = centered ? GPoint(region.size.w / 2, region.size.h / 2) : GPoint(0, 0);
 
+  int start_x = region.origin.x - offset.x;
+  int start_y = region.origin.y - offset.y;
+  int end_x = start_x + region.size.w;
+  int end_y = start_y + region.size.h;
 
-  for(int x = region.origin.x; x < region.origin.x + region.size.w; x++) {
-    GPoint actual_pos = GPoint(x - offset.x, 0);
-    if (actual_pos.x > bimd->size.w) {
-      break;
-    }
-    if (actual_pos.x < 0) {
-      continue;
-    }
-    for(int y = region.origin.y; y < region.origin.y + region.size.h; y++) {
-      actual_pos.y = y - offset.y;
-      if (actual_pos.y > bimd->size.h) {
-        break;
+  // Clamp to image bounds
+  if (start_x < 0) start_x = 0;
+  if (start_y < 0) start_y = 0;
+  if (end_x > bimd->size.w) end_x = bimd->size.w;
+  if (end_y > bimd->size.h) end_y = bimd->size.h;
+
+  for (int y = start_y; y < end_y; y++) {
+    size_t row_start = (size_t)bimd->size.w * y + start_x;
+    size_t row_end = (size_t)bimd->size.w * y + end_x;
+
+    // If clearing full bytes, do it in chunks
+    size_t byte_start = row_start >> 3;
+    size_t byte_end = row_end >> 3;
+
+    if ((row_start & 7) == 0 && (row_end & 7) == 0) {
+      // Perfect byte alignment: clear entire bytes
+      memset(&bimd->data[byte_start], 0, byte_end - byte_start);
+    } else {
+      // Fallback: clear bit-by-bit for partial bytes
+      for (int x = start_x; x < end_x; x++) {
+        binary_image_mask_data_set_bit(bimd, x, y, false);
       }
-      if (actual_pos.y < 0) {
-        continue;
-      }
-      binary_image_mask_data_set_bit(bimd, actual_pos.x, actual_pos.y, false);
     }
   }
 }
 
 bool binary_image_mask_data_get_pixel(BinaryImageMaskData* bimd, int x, int y) {
-  uint8_t byte = bimd->data[(bimd->size.w * y + x) / 8];
-  int reminder = (bimd->size.w * y + x) % 8;
-  return ((byte >> reminder) & 1) == 1;
+  size_t index = bimd->size.w * y + x;
+  uint8_t byte = bimd->data[index >> 3];
+  return ((byte >> (index & 7)) & 1);
 }
 
 bool binary_image_mask_data_is_filled_or_adjacent(BinaryImageMaskData* bimd, int x, int y, int margin) {
-  for (int yy = max(y - margin, 0); yy <= min(y + margin, bimd->size.h - 1); yy++) {
-    for (int xx = max(x - margin, 0); xx <= min(x + margin, bimd->size.w - 1); xx++) {
+  for (int yy = MAX(y - margin, 0); yy <= MIN(y + margin, bimd->size.h - 1); yy++) {
+    for (int xx = MAX(x - margin, 0); xx <= MIN(x + margin, bimd->size.w - 1); xx++) {
       if (binary_image_mask_data_get_pixel(bimd, xx, yy)) {
         return true;
       }
@@ -90,30 +100,23 @@ bool binary_image_mask_data_is_filled_or_adjacent(BinaryImageMaskData* bimd, int
 }
 
 void binary_image_mask_data_draw(BinaryImageMaskData* dest, BinaryImageMaskData* src, GPoint dest_pos, GRect src_reg, bool centered) {
-  GPoint offset = GPoint(0, 0);
-  if (centered) {
-    offset = GPoint(src_reg.size.w / 2, src_reg.size.h / 2);
-  }
+  GPoint offset = centered ? GPoint(src_reg.size.w / 2, src_reg.size.h / 2) : GPoint(0, 0);
 
-  for (int x = 0; x < src_reg.size.w; x++) {
-    GPoint actual_dest_pos = GPoint(dest_pos.x + x - offset.x, 0);
-    if (actual_dest_pos.x >= dest->size.w) {
-      break;
-    }
-    if (actual_dest_pos.x < 0) {
-      continue;
-    }
-    for(int y = 0; y < src_reg.size.h; y++) {
-      actual_dest_pos.y = dest_pos.y + y - offset.y;
-      if (actual_dest_pos.y >= dest->size.h) {
-        break;
-      }
-    if (actual_dest_pos.y < 0) {
-      continue;
-    }
-      bool val = binary_image_mask_data_get_pixel(src, src_reg.origin.x + x, src_reg.origin.y + y);
-      if (val) {
-        binary_image_mask_data_set_bit(dest, actual_dest_pos.x, actual_dest_pos.y, val);
+  int start_x = src_reg.origin.x;
+  int start_y = src_reg.origin.y;
+  int end_x = start_x + src_reg.size.w;
+  int end_y = start_y + src_reg.size.h;
+
+  for (int y = start_y; y < end_y; y++) {
+    int dest_y = dest_pos.y + (y - start_y) - offset.y;
+    if (dest_y < 0 || dest_y >= dest->size.h) continue;
+
+    for (int x = start_x; x < end_x; x++) {
+      int dest_x = dest_pos.x + (x - start_x) - offset.x;
+      if (dest_x < 0 || dest_x >= dest->size.w) continue;
+
+      if (binary_image_mask_data_get_pixel(src, x, y)) {
+        binary_image_mask_data_set_bit(dest, dest_x, dest_y, true);
       }
     }
   }
--- src/c/utils/MathUtils.h
+++ src/c/utils/MathUtils.h
@@ -1,15 +1,26 @@
 #pragma once
 #include <pebble.h>
 
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define TRIG_SCALE (1.0f / (float)TRIG_MAX_ANGLE)
+
 uint32_t two_point_distance(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2);
+
 uint32_t isqrt(uint32_t n);
+
 GPoint polar_to_point(int angle, int distance);
+
 GPoint add_points(GPoint a, GPoint b);
+
 GPoint polar_to_point_offset(GPoint offset, int angle, int distance);
+
 float slope_from_two_points(GPoint a, GPoint b);
-int min(int a, int b);
-int max(int a, int b);
+
 double fmod(double a, double b);
+
 float sine(int32_t angle);
+
 float cosine(int32_t angle);
+
 float tangent(int32_t angle);
